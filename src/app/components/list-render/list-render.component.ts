b87394fb93dc62f524387ef3b14a710494efbd4d
Sergio Alves <76402373+Sergio81alves@users.noreply.github.com>
2023-02-05 23:03:35 -0300

    1% melhor

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -13,6 +13,13 @@ export class ListRenderComponent implements OnInit {
     {nome: 'marcia', tipo: 'Dog"a"'},
   ]
 
+  produtos = [
+    {name: "Dipirona", ml: "20ml", tarja: "Branca", preco: "15"},
+    {name: "Novalgina", ml: "15ml", tarja: "Branca", preco: "16"},
+    {name: "Paracetamol", ml: "25ml", tarja: "Branca", preco: "20"},
+    {name: "Decadrom", ml: "20ml", tarja: "Preta", preco: "45"},
+    {name: "defratr", ml: "10ml", tarja: "vermelha", preco: "100"},
+  ]
   ngOnInit(): void {
   }
 
