54af3b4c0458d57e444e63f090f210dad5245416
Sergio Alves <76402373+Sergio81alves@users.noreply.github.com>
2023-02-07 23:17:15 -0300

    arquivos do two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,7 +37,7 @@ import { TwoWayComponent } from './components/two-way/two-way.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/two-way/two-way.component.ts
+++ src/app/components/two-way/two-way.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TwoWayComponent implements OnInit {
 
+  nome: string = "";
   constructor() { }
 
   ngOnInit(): void {
