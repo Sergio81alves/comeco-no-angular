85dfec5cb323547c9a23405745c1a9abf57f5f78
Sergio Alves <76402373+Sergio81alves@users.noreply.github.com>
2023-02-09 22:58:08 -0300

    atualizando o router Link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { TwoWayComponent } from './components/two-way/two-way.component';
 
 const routes: Routes = [
   {path: "", component: FirstComponentComponent},
   {path: "list", component: ListRenderComponent},
+  {path: "emiter", component: EmitterComponent},
+  {path: "two", component: TwoWayComponent},
+  {path: "eventos", component: EventosComponent},
+  {path: "if", component: IfRenderComponent},
 ];
 
 @NgModule({
