66cd95557db9a3fe748f29236260eed5454bda1e
Sergio Alves <76402373+Sergio81alves@users.noreply.github.com>
2023-02-10 23:18:43 -0300

    requisição Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {HttpClientModule} from "@angular/common/http"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +38,8 @@ import { TwoWayComponent } from './components/two-way/two-way.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, FormsModule
+    AppRoutingModule, FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
