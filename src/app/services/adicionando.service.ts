0de86138dc6e00722d498507e963cee9b6dffbdf
Sergio Alves <76402373+Sergio81alves@users.noreply.github.com>
2023-02-16 23:17:39 -0300

    produtos no banco de dados

--- src/app/services/adicionando.service.ts
+++ src/app/services/adicionando.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Produtos } from '../Produtos';
+import { HttpClient } from '@angular/common/http';
+import { observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
